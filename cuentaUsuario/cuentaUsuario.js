f3a7958c9235ec951a45cb411a4196aec7dd2597
JhonPoma <jpm11797@gmail.com>
2023-10-28 12:14:39 -0500

    ihc

---
+++ cuentaUsuario/cuentaUsuario.js
@@ -0,0 +1,26 @@
+
+const $iconUsuario = document.getElementById('iconUsuario');
+const $menuOpciones = document.getElementById('menuOpciones');
+
+$iconUsuario.addEventListener('click', () => {
+    //alert("hola");
+  // Muestra o oculta el menú de opciones 
+  if ($menuOpciones.style.display === 'none' || $menuOpciones.style.display === '') {
+    $menuOpciones.style.display = 'block';
+  } else {
+    $menuOpciones.style.display = 'none';
+  }
+  //alert("sali")
+});
+
+// Cierra el menú de opciones si se hace clic fuera de él
+document.addEventListener('click', (event) => {
+  if (event.target !== $iconUsuario && event.target !== $menuOpciones) {
+    $menuOpciones.style.display = 'none';
+  }
+});
+
+
+
+
+
