b3a83dbf444100349a637e8f6c5bd85f50c7d9cb
JhonPoma <jpm11797@gmail.com>
2023-11-07 19:06:08 -0500

    ihc

--- vistaLogin/login.js
+++ vistaLogin/login.js
@@ -25,6 +25,36 @@ $validaSesion.addEventListener("click", e=>{
 
 
 
+const $btnPass = document.querySelector(".btnpass");
+const $contador = document.getElementById("contador");
+const $btnEnviarCodigo = document.getElementById("btnEnviarCodigo");
+const $mensaje = d.getElementById("msj");
+
+let tiempoRestante = 5; 
+
+$btnPass.addEventListener("click", () => {
+    // Inicia el contador descendente
+    let interval = setInterval(() => {
+        tiempoRestante--;
+        $mensaje.textContent = `Código enviado, verifica tu correo`;
+        $contador.textContent = `Tiempo restante: ${tiempoRestante} segundos`;
+        
+        if (tiempoRestante <= 0) {
+            clearInterval(interval);
+            $btnEnviarCodigo.style.display = "block";
+        }
+    }, 1000);
+});
+
+$btnEnviarCodigo.addEventListener("click", () => {
+    //envío del código nuevamente
+    tiempoRestante = 10;
+    $btnEnviarCodigo.style.display = "none";
+    $contador.textContent = "";
+    $btnPass.click();
+});
+
+
 
 
 
