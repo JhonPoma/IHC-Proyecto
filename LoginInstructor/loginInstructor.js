b84110fcc1ac4c8928787a4818bfdf9857139d66
JhonPoma <jpm11797@gmail.com>
2023-11-14 13:23:51 -0500

    Registrobackend

--- LoginInstructor/loginInstructor.js
+++ LoginInstructor/loginInstructor.js
@@ -1,8 +1,46 @@
 
-const btnIniciarSesionInstructor = document.getElementById("btnValidaSesion");
+// const btnIniciarSesionInstructor = document.getElementById("btnValidaSesion");
 
-btnIniciarSesionInstructor.addEventListener("click",e=>{
-    window.location.href = "../LoginInstructor/principalInstructor.html";
-});
+// btnIniciarSesionInstructor.addEventListener("click",e=>{
+//     window.location.href = "../LoginInstructor/principalInstructor.html";
+// });
+
+
+const d = document;
+const $loginForm = d.getElementById('login-form');
+const $validaSesion = d.getElementById('btnValidaSesion');
+const $textCorreo = d.getElementById('correo');
+const $textPass = d.getElementById('pass');
+
+
+$validaSesion.addEventListener("click", async(e)=>{
+  e.preventDefault();
+  const correo = $textCorreo.value;
+  const pass = $textPass.value;
+
+  //Solicitud POST al servidor
+  try{
+    const response = await fetch('http://localhost:5500/validar',{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify({correo,pass}),
+    });
+    //alert("jhonFronted");
+    if(response.ok){
+      window.location.href="../LoginInstructor/principalInstructor.html";
+    }else{
+      //alert("credenciales incorrectAss-FRONTED");
+      $textCorreo.value = "";
+      $textPass.value = "";
+    }
+  }
+  catch (error){
+    console.error(error);
+  }
+})
+
+
 
 
--- vistaRegistro/registro.js
+++ vistaRegistro/registro.js
@@ -1,33 +1,50 @@
 
 d=document;
 const $btnRegistro = d.getElementById("btnRegistrarse");
+const $usuarioValor = d.getElementById("usuario");
 const $correoValor = d.getElementById("correo");
 const $passValor = d.getElementById("pass");
 
+const $mensajeExito = d.getElementById("mensajeRegistroExito"); 
+const $mensajeError = d.getElementById("mensajeRegistroError"); 
 
 $btnRegistro.addEventListener("click", async (e) =>{
     e.preventDefault();
     const correo = $correoValor.value;
     const pass = $passValor.value;
-
-    try{
-        const respuesta = await fetch('http://localhost:5500/registrar',{
-            method:'POST',
-            headers:{
-                "Content-Type":"application/json",
-            },
-            body: JSON.stringify({correo,pass}),
-        });
-        if(respuesta.ok){
-            window.location.href="../cuentaUsuario/cuentaUsuario.html";
+    if(correo.length>0 && pass.length>0){
+        try{
+            const respuesta = await fetch('http://localhost:5500/registrar',{
+                method:'POST',
+                headers:{
+                    "Content-Type":"application/json",
+                },
+                body: JSON.stringify({correo,pass}),
+            });
+            if(respuesta.ok){
+                if($mensajeExito.style.display==="none" || $mensajeExito.style.display===""){
+                    $mensajeExito.style.display="block";
+                }
+                $mensajeExito.style.display="block";
+                window.location.href="../cuentaUsuario/cuentaUsuario.html";
+            }
+            else{
+                if($mensajeError.style.display==="none" || $mensajeError.style.display===""){
+                    $mensajeError.style.display="block";
+                }
+                document.addEventListener("click", (e)=>{
+                    $mensajeError.style.display="none";
+                })
+                $correoValor.value = "";
+                $passValor.value = "";
+                $usuarioValor.value="";
+            }
+        }catch (error){
+            console.error(error);
         }
-        else{
-            $correoValor.value = "";
-            $passValor.value = "";
-        }
-    }catch (error){
-        console.error(error);
     }
+
+
 });
 
 
